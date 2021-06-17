43d3ddba20c45615e677d84032b08d392ee5d255
David Poulin <dpoulin@datumtc.ca>
2021-06-16 20:54:05 -0400

    Updated copyright headers for new line width (120 char)

--- dv/uvml_riscv_csr/src/uvml_riscv_csr_tdefs.sv
+++ dv/uvml_riscv_csr/src/uvml_riscv_csr_tdefs.sv
@@ -1,19 +1,12 @@
-// 
 // Copyright 2021 Datum Technology Corporation
+// 
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
-// 
-// Licensed under the Solderpad Hardware License v 2.1 (the "License"); you may
-// not use this file except in compliance with the License, or, at your option,
-// the Apache License version 2.0. You may obtain a copy of the License at
-// 
-//     https://solderpad.org/licenses/SHL-2.1/
-// 
-// Unless required by applicable law or agreed to in writing, any work
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-// 
+// Licensed under the Solderpad Hardware License v 2.1 (the "License"); you may not use this file except in compliance
+// with the License, or, at your option, the Apache License version 2.0.  You may obtain a copy of the License at
+//                                        https://solderpad.org/licenses/SHL-2.1/
+// Unless required by applicable law or agreed to in writing, any work distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations under the License.
 
 
 `ifndef __UVML_RISCV_CSR_TDEFS_SV__
@@ -54,7 +47,7 @@ typedef enum int unsigned {
  */
 typedef enum bit [1:0] {
    UVML_RISCV_CSR_MTVEC_MODE_DIRECT     = 0, ///< All exceptions set pc to BASE.
-   UVML_RISCV_CSR_MTVEC_MODE_VECTORED   = 1, ///< Asynchronous interrupts set pc to BASE+4×cause.
+   UVML_RISCV_CSR_MTVEC_MODE_VECTORED   = 1, ///< Asynchronous interrupts set pc to BASE+4ï¿½cause.
    UVML_RISCV_CSR_MTVEC_MODE_RESERVED_0 = 2, ///< Reserved
    UVML_RISCV_CSR_MTVEC_MODE_RESERVED_1 = 3  ///< Reserved
 } uvml_riscv_csr_mtvec_mode_enum;
@@ -64,7 +57,7 @@ typedef enum bit [1:0] {
  */
 typedef enum bit [1:0] {
    UVML_RISCV_CSR_STVEC_MODE_DIRECT     = 0, ///< All exceptions set pc to BASE.
-   UVML_RISCV_CSR_STVEC_MODE_VECTORED   = 1, ///< Asynchronous interrupts set pc to BASE+4×cause.
+   UVML_RISCV_CSR_STVEC_MODE_VECTORED   = 1, ///< Asynchronous interrupts set pc to BASE+4ï¿½cause.
    UVML_RISCV_CSR_STVEC_MODE_RESERVED_0 = 2, ///< Reserved
    UVML_RISCV_CSR_STVEC_MODE_RESERVED_1 = 3  ///< Reserved
 } uvml_riscv_csr_stvec_mode_enum;
